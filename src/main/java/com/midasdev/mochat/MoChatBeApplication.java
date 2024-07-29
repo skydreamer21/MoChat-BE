7f335c1e43300cf5dfbcb98349ba792fcb273d2e
skydreamer21 <skydreamer210@gmail.com>
2024-07-29 23:20:49 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/midasdev/mochat/MoChatBeApplication.java
@@ -0,0 +1,13 @@
+package com.midasdev.mochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoChatBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoChatBeApplication.class, args);
+    }
+
+}

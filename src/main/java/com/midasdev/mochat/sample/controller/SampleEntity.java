d6d2ad165be2dcf481ddb092e9947704968b9a7e
skydreamer <skydreamer210@gmail.com>
2024-09-06 00:13:14 +0900

    fix: sample 엔티티 id 생성 전략 수정

--- src/main/java/com/midasdev/mochat/sample/controller/SampleEntity.java
+++ src/main/java/com/midasdev/mochat/sample/controller/SampleEntity.java
@@ -12,7 +12,7 @@ import lombok.*;
 public class SampleEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
